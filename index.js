e3fb59bb330f914df1addc77d62641d64724f3c7
Alessandro DG <alessandrodigiacomantonio.dev@gmail.com>
2026-02-11 16:34:49 +0100

    chore: order section

--- index.js
+++ index.js
@@ -1,19 +1,65 @@
 import { menu } from './menu.js';
 
+let orderItems = JSON.parse(localStorage.getItem('orderItems')) || [];
+
 const menuHtml = menu.map((item) => {
     return `
-        <div class="card">
-            <img class="card-img" src="${item.image}" alt="${item.title}">
-            <div class="card-text">
-                <h2>${item.title}</h2>
-                <p class="card-ingredients">${item.ingredients}</p>
-                <p>${item.price}</p>
+        <div class="menu-card">
+            <div class="menu-card-img-container">
+                <img class="menu-card-img" src="${item.image}" alt="${item.title}">
             </div>
-            <button class="card-btn">+</button>
+            <div class="menu-card-content">
+                <h2 class="menu-card-title">${item.title}</h2>
+                <p class="menu-card-ingredients">${item.ingredients}</p>
+                <p class="menu-card-price">€${item.price}</p>
+            </div>
+            <button class="menu-card-btn" data-item="${item.title}">+</button>
         </div>
     `;
 }).join('');
 
-console.log(menuHtml);
-
 document.querySelector('.menu').innerHTML = menuHtml;
+renderOrderItems();
+
+document.addEventListener('click', (event) => {
+    if(event.target.classList.contains('menu-card-btn')) {
+        const itemTitle = event.target.dataset.item;
+
+        if( ! orderHasItem(itemTitle) ){
+            orderItems.push(menu.find(item => item.title === itemTitle));
+            localStorage.setItem('orderItems', JSON.stringify(orderItems));
+        }
+
+        renderOrderItems();
+    }
+
+    if(event.target.classList.contains('order-item-remove-btn')) {
+        const itemTitle = event.target.dataset.item
+        orderItems = orderItems.filter(item => item.title !== itemTitle);
+        localStorage.setItem('orderItems', JSON.stringify(orderItems));
+
+        renderOrderItems();
+    }
+
+})
+
+function orderHasItem(itemTitle) {
+    return orderItems.find(item => item.title === itemTitle);
+}
+function renderOrderItems(){
+    if(!orderItems.length) {
+        document.querySelector('.order').classList.add('hidden');
+        return;
+    }
+
+    const totalAmount = orderItems.reduce((acc, item) => acc + parseInt(item.price), 0);
+    document.querySelector('.order-items').innerHTML = orderItems.map(item => `
+        <li class="order-item">
+            <p class="order-item-title">${item.title}</p>
+            <button class="order-item-remove-btn" data-item="${item.title}">remove</button>
+            <span class="order-item-price">€${item.price}</span>
+        </li>
+    `).join('')
+    document.querySelector('.total-amount').textContent = '€'+totalAmount;
+    document.querySelector('.order').classList.remove('hidden');
+}
--- menu.js
+++ menu.js
@@ -2,19 +2,19 @@ export const menu = [
     {
         title: 'Pizza',
         ingredients: 'pepperoni, mushrooms, mozzarella',
-        price: '$14',
+        price: '14',
         image: '/images/pizza.png'
     },
     {
         title: 'Hamburger',
         ingredients: 'beef, cheese, lettuce',
-        price: '$12',
+        price: '12',
         image: '/images/hamburger.png'
     },
     {
         title: 'Beer',
         ingredients: 'grain, hops, yeast, water',
-        price: '$12',
+        price: '12',
         image: '/images/beer.png'
     },
 ]

a1f998ef823b75189faf1c95fd6dcda4ce573a26
Alessandro DG <alessandrodigiacomantonio.dev@gmail.com>
2026-02-11 14:17:09 +0100

    chore: home

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ import { menu } from './menu.js';
 const menuHtml = menu.map((item) => {
     return `
         <div class="card">
-            <img src="${item.image}" alt="${item.title}">
-            <h2>${item.title}</h2>
-            <p class="ingredients">${item.ingredients}</p>
-            <p>${item.price}</p>
+            <img class="card-img" src="${item.image}" alt="${item.title}">
+            <div class="card-text">
+                <h2>${item.title}</h2>
+                <p class="card-ingredients">${item.ingredients}</p>
+                <p>${item.price}</p>
+            </div>
+            <button class="card-btn">+</button>
         </div>
     `;
 }).join('');
